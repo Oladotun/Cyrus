2eee2ea1a907e1be4f76605bbc85e391e2889497
Dotun Opasina <opasina@Dotuns-MacBook-Air.local>
2016-02-16 07:55:45 -0500

    Initial Commit

---
+++ CyrusTheGreat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CyrusTheGreat
+//
+//  Created by Dotun Opasina on 2/16/16.
+//  Copyright (c) 2016 Dotun Opasina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
