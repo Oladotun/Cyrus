c0ac9fe73f81f9e6887b7e6b23bac9eae43fa93e
Dotun Opasina <opasina@Dotuns-MacBook-Air.local>
2016-03-02 11:34:07 -0500

    Cleaned up welcome page and set auto layout for the view, Removed typing effect from screen and included fade in

--- CyrusTheGreat/FrontPageViewController.swift
+++ CyrusTheGreat/FrontPageViewController.swift
@@ -12,55 +12,20 @@ class FrontPageViewController: UIViewController {
 
     @IBOutlet weak var logoCyrus: UIImageView!
     
-    var cyrusText = "Hi there,my name is Cyrus\n Today,my goal is to connect you with people who share similar interest with you so you can learn and discuss about those views.\n\nKindly Sign Up or Login to start connecting.\n"
+    var cyrusText = "Hi there,my name is Cyrus.\n\nToday, my goal is to connect you with people who you share similar interest with, so you can learn from their views.\n\nKindly Sign Up or Login to start connecting.\n"
+    
+    let paragraphStyle = NSMutableParagraphStyle()
+    
     
     var cyrusIntroWords: [Character]!
     var myCounter = 0
     var timer:NSTimer?
 
+    @IBOutlet weak var blockLabel: UILabel!
     @IBOutlet weak var cyrusIntro: UILabel!
     @IBOutlet weak var signUp: UIButton!
     @IBOutlet weak var login: UIButton!
-    var wordTimeInterval = 0.09
-    
-    func fireTimer(startTime:Float){
-        var timeSchedule = NSTimeInterval(0.5 + startTime)
-        
-        timer = NSTimer.scheduledTimerWithTimeInterval(timeSchedule, target: self, selector: "typeLetter", userInfo: nil, repeats: true)
-        
-        cyrusIntroWords = Array(self.cyrusText)
-        
-        
-        var endDouble = timeSchedule + (Double(cyrusIntroWords.count) * wordTimeInterval) + 0.3
-        var endTime = NSTimeInterval(endDouble)
-       
-        UIView.animateWithDuration(1.5, delay: endTime, options: .CurveEaseInOut, animations: {
-            self.login.alpha = 1.0
-            self.signUp.alpha = 1.0
-        }, completion: nil)
-    }
-    
-    
-    func typeLetter(){
-        
-        // whent the TimeInterval is 0.0 implies we need to escape the word
-        if wordTimeInterval == 0.0 {
-            cyrusIntro.text = cyrusText
-            timer?.invalidate()
-            self.signUp.layer.removeAllAnimations()
-            self.login.layer.removeAllAnimations()
-            self.login.alpha = 1.0
-            self.signUp.alpha = 1.0
-            
-        } else if myCounter < cyrusIntroWords.count {
-            cyrusIntro.text = cyrusIntro.text! + String(cyrusIntroWords[myCounter])
-            timer?.invalidate()
-            timer = NSTimer.scheduledTimerWithTimeInterval(NSTimeInterval(wordTimeInterval), target: self, selector: "typeLetter", userInfo: nil, repeats: false)
-        } else {
-            timer?.invalidate()
-        }
-        myCounter++
-    }
+
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         logoCyrus.image = UIImage(named:"cyrus")
@@ -70,7 +35,16 @@ class FrontPageViewController: UIViewController {
         UIView.animateWithDuration(0.5, delay: 0.0, options: .CurveEaseInOut, animations: {
             self.logoCyrus.alpha = 1.0
         }, completion: nil)
-        fireTimer(0.5)
+        
+        UIView.animateWithDuration(1.0, delay: 1.0, options: .CurveEaseInOut, animations: {
+            self.cyrusIntro.alpha = 1
+            }, completion: nil)
+        
+        UIView.animateWithDuration(1.0, delay: 2.0, options: .CurveEaseInOut, animations: {
+            self.login.alpha = 1.0
+            self.signUp.alpha = 1.0
+            }, completion: nil)
+        
         
     }
     
@@ -86,20 +60,27 @@ class FrontPageViewController: UIViewController {
         login.layer.borderColor = signUp.layer.borderColor
         login.alpha = 0.0
         
-        let tap = UITapGestureRecognizer(target: self, action: Selector("tapFunction:"))
-        cyrusIntro.addGestureRecognizer(tap)
-        cyrusIntro.userInteractionEnabled = true
+        // Used in aligning text messages
+        paragraphStyle.alignment = NSTextAlignment.Justified
+        
+        let attributedString = NSAttributedString(string: cyrusText,
+            attributes: [
+                NSParagraphStyleAttributeName: paragraphStyle,
+                NSBaselineOffsetAttributeName: NSNumber(float: 0)
+            ])
+        
+        cyrusIntro.attributedText = attributedString
+        cyrusIntro.font = UIFont(name: "Helvetica-Light", size: 15.0)
+        cyrusIntro.alpha = 0.0
+        blockLabel.text = ""
+        
+        
         
 
         // Do any additional setup after loading the view.
     }
     
-    func tapFunction(sender:UITapGestureRecognizer) {
-        print("Tap Selected\n")
-        self.wordTimeInterval = 0.0
     
-        
-    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
