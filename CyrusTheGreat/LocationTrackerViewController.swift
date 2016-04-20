45d67afafcc87e35047aadd031e19f4e43d65094
Dotun Opasina <opasina@dotuns-macbook-air.local>
2016-04-20 08:57:21 -0400

    Added ability for user to locate themselves through Cyrus

--- CyrusTheGreat/LocationTrackerViewController.swift
+++ CyrusTheGreat/LocationTrackerViewController.swift
@@ -10,12 +10,19 @@ import UIKit
 import GoogleMaps
 
 class LocationTrackerViewController: UIViewController {
+    
+    let locationManager = CLLocationManager()
 
     @IBOutlet weak var currMapView: GMSMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // Find current Location of user
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,7 +30,6 @@ class LocationTrackerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
     /*
     // MARK: - Navigation
 
@@ -35,3 +41,61 @@ class LocationTrackerViewController: UIViewController {
     */
 
 }
+
+// MARK: - CLLocationManagerDelegate
+//1
+extension LocationTrackerViewController: CLLocationManagerDelegate {
+    //2
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        // 3
+        if status == .AuthorizedWhenInUse {
+            // 4
+            locationManager.startUpdatingLocation()
+            // 5
+            currMapView.myLocationEnabled = true
+            currMapView.settings.myLocationButton = true
+        }
+    }
+    
+    // 6
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            
+            let home = CLLocationCoordinate2DMake(39.3435, -75.5846)
+            
+            // 7
+            currMapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 12, bearing: 0, viewingAngle: 0)
+            let markPosition = GMSMarker(position: location.coordinate)
+            markPosition.title = "My Position"
+            markPosition.map = currMapView
+            
+            let otherPosition = GMSMarker(position: home)
+            otherPosition.title = "Position Home"
+            otherPosition.map = currMapView
+//            currMapView.camera = GMSCameraPosition(target: home, zoom: 5, bearing: 0, viewingAngle: 0)
+            
+            //8
+            locationManager.stopUpdatingLocation()
+        }
+    }
+}
+
+
+// MARK: - GoogleMapDelegate
+
+extension LocationTrackerViewController: GMSMapViewDelegate {
+    
+    func mapView(mapView: GMSMapView, didChangeCameraPosition position: GMSCameraPosition) {
+//        reverseGeocodeCoordinate(position.target)
+    }
+    
+    
+    
+//    func mapView(mapView: GMSMapView, willMove gesture: Bool) {
+//        adressLabel.lock()
+//    }
+    
+    
+    
+}
+
