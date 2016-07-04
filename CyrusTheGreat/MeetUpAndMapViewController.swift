9ea46fbdb7d07b96cd4f2cd0c477fcaa650ebfa6
Dotun Opasina <opasina@dotuns-macbook-air.local>
2016-07-03 23:08:51 -0400

    Added state restoration to MeetUpAndMapViewController, referencing children view controllers

--- CyrusTheGreat/MeetUpAndMapViewController.swift
+++ CyrusTheGreat/MeetUpAndMapViewController.swift
@@ -19,14 +19,17 @@ class MeetUpAndMapViewController: UIViewController {
     var placeAddress:String!
     var destinationLocation:CLLocation!
     var meetUpPage:MeetUpPageViewController!
+    var mapPage: MapTrackViewController!
     
     var firebaseMapManager:FirebaseMapManager!
     var firebaseMeetupInfoManager:FirebaseInfoMeetUpManager!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        restorationIdentifier = "MeetUpAndMapViewControllerId"
+        restorationClass = MeetUpAndMapViewController.self
         self.meetUpInfoContainer.alpha = 1
         self.locationContainer.alpha = 0
+        print("Called me again")
 
         // Do any additional setup after loading the view.
     }
@@ -35,6 +38,88 @@ class MeetUpAndMapViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // Restore Info
+    override func encodeRestorableStateWithCoder(coder: NSCoder) {
+        //1
+        
+        if let meetTime = time {
+            coder.encodeObject(meetTime, forKey: "CyrusChatTime")
+        }
+        if let destiny = destination {
+            coder.encodeObject(destiny, forKey: "CyrusDestination")
+        }
+        if let placement = placeAddress {
+            coder.encodeObject(placement, forKey: "CyrusPlacedAddress")
+        }
+        
+        if let locateDestiny = destinationLocation {
+            coder.encodeObject(locateDestiny, forKey: "CyrusDestinyLocation")
+        }
+        
+        if let firebase = firebaseMeetupInfoManager {
+            coder.encodeObject(firebase, forKey: "firebaseMeetUpInfo")
+        }
+        
+        if let firebaseMap = firebaseMapManager {
+            coder.encodeObject(firebaseMap, forKey: "firebaseMapManager")
+        }
+        
+
+        if let vc = mapPage {
+            coder.encodeObject(vc, forKey: "mapPageInfo")
+        }
+        
+        if let vc = meetUpPage {
+            coder.encodeObject(vc, forKey: "meetUpPageViewController")
+        }
+        
+//        self.viewContr
+        //2
+        super.encodeRestorableStateWithCoder(coder)
+//        meetUpInfoContainer.
+    }
+    
+    override func decodeRestorableStateWithCoder(coder: NSCoder) {
+        
+        if let ct = coder.decodeObjectForKey("CyrusChatTime") {
+            time = ct as! String
+        }
+        if let dest = coder.decodeObjectForKey("CyrusDestination") {
+            destination = dest as! String
+        }
+        if let place = coder.decodeObjectForKey("CyrusPlacedAddress") {
+            placeAddress = place as! String
+            
+        }
+        if let destLC = coder.decodeObjectForKey("CyrusDestinyLocation") {
+            destinationLocation = destLC as! CLLocation
+        }
+        if let firebaseInfo = coder.decodeObjectForKey("firebaseMeetUpInfo") {
+            firebaseMeetupInfoManager = firebaseInfo as! FirebaseInfoMeetUpManager
+            
+        }
+        if let firebaseMap = coder.decodeObjectForKey("firebaseMapManager") {
+            firebaseMapManager = firebaseMap as! FirebaseMapManager
+        }
+        
+        super.decodeRestorableStateWithCoder(coder)
+    }
+    
+    override func applicationFinishedRestoringState() {
+        // Final configuration goes here.
+        // Load images, reload data, e. t. c.
+        guard let _ = firebaseMeetupInfoManager else { print("info error nil");return }
+        guard let _ = firebaseMapManager else {print("bae map manager error"); return}
+
+    }
+    
+    static func viewControllerWithRestorationIdentifierPath(identifierComponents: [AnyObject], coder: NSCoder) -> UIViewController? {
+        let vc = MeetUpAndMapViewController()
+        return vc
+    }
+    
+    
     
 
     @IBAction func showComponent(sender: UISegmentedControl) {
@@ -65,22 +150,29 @@ class MeetUpAndMapViewController: UIViewController {
         
         if (segue.identifier == "meetWearingSegue") {
 //            print("Called wearing segue")
-            let childViewController = segue.destinationViewController as! MeetUpPageViewController
-            childViewController.time = time
-            childViewController.destination = destination
-            childViewController.firebaseMeetUpManager = firebaseMeetupInfoManager
             
-            meetUpPage = childViewController // use this map controller as delegate for map track
+                let childViewController = segue.destinationViewController as! MeetUpPageViewController
+                childViewController.time = time
+                childViewController.destination = destination
+                childViewController.firebaseMeetUpManager = firebaseMeetupInfoManager
+                
+                meetUpPage = childViewController // use this map controller as delegate for map track
+            
+            
             
         }
         
         if (segue.identifier == "mapSegue") {
 //            print("Called map segue")
-            let childViewController = segue.destinationViewController as! MapTrackViewController
-            childViewController.addressString = placeAddress
-            childViewController.destinationLocation = destinationLocation
-            childViewController.mapProtocol = meetUpPage
-            childViewController.firebaseMapManager = firebaseMapManager
+            
+                let childViewController = segue.destinationViewController as! MapTrackViewController
+                childViewController.addressString = placeAddress
+                childViewController.destinationLocation = destinationLocation
+                childViewController.mapProtocol = meetUpPage
+                childViewController.firebaseMapManager = firebaseMapManager
+                mapPage = childViewController
+          
+            
             
 //            meetUpInfoContainer
             
